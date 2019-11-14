33773e7eb4cdd2be46493de7528a44bf2fdea153
mfalcetta <44413306+mfalcetta@users.noreply.github.com>
2019-11-14 15:49:38 -0600

    Add page route and products

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'page/show'
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :suppliers, only: :index
-  resources :regions
+  resources :products, only: :index
+  resources :page, only: :show
 
-  root to: 'suppliers#index'
+  root to: 'products#index'
 end
