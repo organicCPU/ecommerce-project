975ccb8e8b677f361daf3bf3cab312d5034e9dfa
mfalcetta <44413306+mfalcetta@users.noreply.github.com>
2019-11-14 15:50:10 -0600

    Add administrative control to all tables

---
+++ app/admin/order_details.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register OrderDetail do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :order_number, :price, :total, :orders_id, :products_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:order_number, :price, :total, :orders_id, :products_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
