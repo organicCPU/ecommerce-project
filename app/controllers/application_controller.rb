51c19b4a8b78a4ad89d1bbfad1cf9943c5ca049b
mfalcetta <44413306+mfalcetta@users.noreply.github.com>
2019-11-21 16:56:29 -0600

    Add page support for navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  # private method that loads stuff
+
+  private
+
+  def navigation_pages
+    @navigation_pages = Page.where(show_in_navbar: true)
+  end
+  helper_method :navigation_pages
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :suppliers, only: [:index, :show]
-  resources :categories, only: [:index, :show]
-  resources :products, only: [:index, :show]
+  resources :suppliers, only: %i[index show]
+  resources :categories, only: %i[index show]
+  resources :products, only: %i[index show]
   get ':permalink', to: 'pages#permalink', as: 'pages'
 
   root to: 'products#index'
