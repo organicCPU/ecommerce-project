43ce45f47e68c83dfb2eab22465be02c51f88f6a
mfalcetta <44413306+mfalcetta@users.noreply.github.com>
2019-11-22 08:55:11 -0600

    Add basic cart functionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,43 @@
 # frozen_string_literal: true
 
 class ProductsController < ApplicationController
+  before_action :initialize_session
+
   def index
     @products = Product.order(:name).page params[:page]
   end
 
   def show
     @product = Product.find(params[:id])
+  end
+
+  def add_to_cart
+    product_id = parse_id
+
+    session[:cart] << product_id unless session[:cart].include?(product_id)
+
+    redirect_to root_path
+  end
+
+  def remove_from_cart
+    product_id = parse_id
+    session[:cart].delete(product_id)
+
+    redirect_to root_path
+  end
+
+  private
+
+  def parse_id
+    params[:id].to_i
+  end
+
+  def initialize_session
+    session[:cart] ||= []
+    load_cart
+  end
+
+  def load_cart
+    @cart = Product.find(session[:cart])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
   resources :suppliers, only: %i[index show]
   resources :categories, only: %i[index show]
   resources :products, only: %i[index show]
+
+  post '/products/add_to_cart/:id', to: 'products#add_to_cart', as: 'add_to_cart'
+  delete '/products/remove_from_cart/:id', to: 'products#remove_from_cart', as: 'remove_from_cart'
+
   get ':permalink', to: 'pages#permalink', as: 'pages'
 
   root to: 'products#index'
