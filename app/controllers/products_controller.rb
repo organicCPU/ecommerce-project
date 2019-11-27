1ce3da0ae5de9e5ccde004a2f7ff2f74ca620a1d
mfalcetta <44413306+mfalcetta@users.noreply.github.com>
2019-11-27 13:43:54 -0600

    Change redirect to redirect back instead of root

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,14 +20,14 @@ class ProductsController < ApplicationController
       session[:cart] << { id: product_id, quantity: 1 }
     end
 
-    redirect_to root_path
+    redirect_back(fallback_location: root_path)
   end
 
   def remove_from_cart
     product_id = parse_id
     session[:cart].delete_if { |hash| hash['id'] == product_id }
 
-    redirect_to root_path
+    redirect_back(fallback_location: root_path)
   end
 
   private
